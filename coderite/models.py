07d18b6bf8eba60c50c8ba126deb40e23979be76
DefaltZ <priyongshupaul@gmail.com>
2021-08-07 18:40:41 +0530

    file upload metrics

--- coderite/models.py
+++ coderite/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.db.models.fields import CharField
 from django_mysql.models import ListCharField
 from django_mysql.models.base import Model
+from django.core.validators import FileExtensionValidator
 
 STATUS = (
     (0, 'Draft'),
@@ -9,7 +10,7 @@ STATUS = (
 )
 # Create your models here.
 class Post(models.Model):
-    image = models.ImageField(upload_to = 'coderite/assets')
+    image = models.FileField(upload_to = 'coderite/assets', validators=[FileExtensionValidator(['svg'])])
     slug = models.SlugField(max_length=200, unique=True)
     title = models.CharField(max_length=250)
     author = models.CharField(max_length=200)
