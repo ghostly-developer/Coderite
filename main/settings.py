41eefe281f36194ef52b77fa03d7c1dd79a0761f
DefaltZ <priyongshupaul@gmail.com>
2021-08-07 09:08:36 +0530

    fixed asset folder path problem

--- main/settings.py
+++ main/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -40,9 +40,7 @@ INSTALLED_APPS = [
     'coderite'
 ]
 
-MEDIA_ROOT = [
-    'coderite/assets/'
-]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
 
 STATICFILES_DIR = [
     'coderite\static'
