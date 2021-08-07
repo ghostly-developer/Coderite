0fb12770bbe3541d5ea795c6b54f6f06022a2b0a
DefaltZ <priyongshupaul@gmail.com>
2021-08-07 18:03:03 +0530

    image database commit

--- main/settings.py
+++ main/settings.py
@@ -40,7 +40,10 @@ INSTALLED_APPS = [
     'coderite'
 ]
 
+STATIC_URL = '/static/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
+MEDIA_URL = '/assets/'
 
 STATICFILES_DIR = [
     'coderite\static'
@@ -124,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
