a6fb00dad2152f0ade7e1e596dcbc6347c6cdba2
DefaltZ <priyongshupaul@gmail.com>
2021-08-07 06:29:23 +0530

    models commit

--- main/settings.py
+++ main/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = 'django-insecure--au*+0wp$!2)dv3rf&yy_x^4otq4g9d+c4%xnr)a-s-w0xu+3+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'coderite'
+]
+
+MEDIA_ROOT = [
+    'coderite/assets/'
 ]
 
 MIDDLEWARE = [
