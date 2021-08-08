4d746017f7cbffa352e2da4280e6c3e5d8dd40c7
DefaltZ <priyongshupaul@gmail.com>
2021-08-08 08:30:11 +0530

    user auth commit

--- coderite/views.py
+++ coderite/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from .models  import Post
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by('-created_on')
@@ -10,8 +12,18 @@ class PostDetail(generic.DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-
-def test(request):  #remove later
-    return render(request, 'base.html')
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
 
 
